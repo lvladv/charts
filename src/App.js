d84f7d20e1a439386d887969366e8d9b3ced20f4
metrics_bot <metrics_bot@rt.ru>
2022-01-17 23:22:29 +0300

    start project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+ 
+
+    </div>
+  );
+}
+
+export default App;
+
+// http://151.248.112.110:3000/api/evergreen/
+// http://151.248.112.110:3000/api/evergreen/tomcapital
